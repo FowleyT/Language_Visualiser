4086806fedd4997deb693b2f027f1d51c7bba480
FowleyT <FowleyT@users.noreply.github.com>
2018-11-30 21:06:02 +0000

    Add initial javascript scaffolding

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = "Iv1.f4ccb61be38ef637";
+const client_secret = "111d311ac213d09ce7cee25266ba68a75135103b"
