43e9f0c7ec8173118c9ac9df669f10582bfcb598
FowleyT <FowleyT@users.noreply.github.com>
2018-12-01 18:31:33 +0000

    Update app.js
    part 1 now working

--- js/app.js
+++ js/app.js
@@ -5,5 +5,26 @@ const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
-const client_id = "Iv1.f4ccb61be38ef637";
-const client_secret = "111d311ac213d09ce7cee25266ba68a75135103b"
+
+
+const getUsers = async (user) => {
+  const api_call = await fetch(`https://api.github.com/users/${user}`);
+
+  const data = await api_call.json();
+  return {data}
+};
+
+const showData =  () => {
+  getUsers(inputValue.value).then((response) => {
+  //  console.log(response);
+
+    nameContainer.innerHTML =`Name: <span class="main__profile-value"> ${response.data.name} </span>`;
+    unContainer.innerHTML =`Username: <span class="main__profile-value"> ${response.data.login} </span>`;
+    reposContainer.innerHTML =`Repos: <span class="main__profile-value"> ${response.data.public_repos} </span>`;
+    urlContainer.innerHTML =`URL: <span class="main__profile-value"> ${response.data.html_url} </span>`;
+  })
+};
+
+searchButton.addEventListener("click", () => {
+  showData();
+})
