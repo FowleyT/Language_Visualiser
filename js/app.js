340284f70d652f56416a85760fa5241e332da0e3
FowleyT <FowleyT@users.noreply.github.com>
2018-12-02 12:31:33 +0000

    add repo list
    having trouble with d3

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+const reposListContainer = document.querySelector(".main__profile-repolist");
+const languageContainer = document.querySelector(".large-7 medium-7 small-12 columns");
 
 
 
@@ -14,9 +16,17 @@ const getUsers = async (user) => {
   return {data}
 };
 
+const getRepos = async (user) => {
+  const api_call = await fetch(`https://api.github.com/users/${user}/repos`);
+
+  const repos = await api_call.json();
+  return {data : repos}
+};
+
+
 const showData =  () => {
   getUsers(inputValue.value).then((response) => {
-  //  console.log(response);
+   //console.log(response);
 
     nameContainer.innerHTML =`Name: <span class="main__profile-value"> ${response.data.name} </span>`;
     unContainer.innerHTML =`Username: <span class="main__profile-value"> ${response.data.login} </span>`;
@@ -25,6 +35,24 @@ const showData =  () => {
   })
 };
 
+const showRepos = () => {
+    getRepos(inputValue.value).then((repos) => {
+      //console.log(repos);
+        for (var i = 0; i < repos.data.length; i++) {
+				     
+              reposListContainer.innerHTML +=`<li class="list-group-item"> ${repos.data[i].name} </li>`;
+        }
+			});
+			}
+
+
+
+
+
 searchButton.addEventListener("click", () => {
   showData();
+  showRepos();
+  showGraph(getRepos(inputValue.value));
+
+
 })
